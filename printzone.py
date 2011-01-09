0782eff554778ce54b494ea2a94117f147b020fa
Jeffrey Forman <jeff@jeffreyforman.net>
2011-01-09 11:53:20 -0500

    improve exception handling, add documentation

--- printzone.py
+++ printzone.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env python
 
+from optparse import OptionParser
+
 import dns.query
 import dns.zone
+import socket
 import sys
-
-from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("--server", dest="dns_server",
                   help="DNS server to query.",
+                  default = "foo",
                   type="string")
 parser.add_option("--zone", dest="dns_zone",
                   help="Zone to print.",
+                  default = False,
                   type="string")
 
 (options, args) = parser.parse_args()
 
 print "Server: %s Zone: %s" % (options.dns_server, options.dns_zone)
 try:
-    z = dns.zone.from_xfr(dns.query.xfr(options.dns_server, options.dns_zone))
+    try:
+        z = dns.zone.from_xfr(dns.query.xfr(options.dns_server, options.dns_zone))
+    except socket.gaierror, e:
+        print "Problems querying DNS server %s: %s\n" % (options.dns_server, e)
+        sys.exit(1)
+
     names = z.nodes.keys()
-    names.sort()
+    names.sort() # Sort the array alphabetically
     zone_xfr_array = []
     for n in names:
         current_record = z[n].to_text(n)
-        for split_record in current_record.split("\n"):
-            zone_xfr_array.append([split_record])
+        for split_record in current_record.split("\n"): # Split the records on the newline
+            zone_xfr_array.append([split_record]) # Add each record to our array
 except dns.exception.FormError:
-    print "The transfer encountered a problem."
+    print "The transfer encountered a problem. Check your zone records.\n"
+    sys.exit(1)
 
-for current_record in zone_xfr_array:
+for current_record in zone_xfr_array: # Iterate and print our DNS records array
     print current_record
 
